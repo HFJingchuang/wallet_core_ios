17baa5592ef58732b09f5b0020cf040dab2fabdb
Astomsh <383063294@qq.com>
2020-07-15 12:11:00 +0800

    update

--- Tests/WalletCoreIOSTests/WalletCoreIOSTests.swift
+++ Tests/WalletCoreIOSTests/WalletCoreIOSTests.swift
@@ -2,35 +2,36 @@ import XCTest
 import BigInt
 
 @testable import chain3swift
+@testable import web3swift
 
 final class WalletCoreIOSTests: XCTestCase {
-    //    func testCreateAccount() {
-    //        let mnemonics = Mnemonics()
-    //        let keystore = try! BIP32Keystore(mnemonics: mnemonics)
-    //        print(keystore.addresses[0])
-    //        Web3.default.keystoreManager = KeystoreManager([keystore])
-    //    }
-    //
-    //    func testImportAccount() {
-    //        let mnemonicsString = "nation tornado double since increase orchard tonight left drip talk sand mad"
-    //        let mnemonics = try! Mnemonics(mnemonicsString)
-    //        let keystore = try! BIP32Keystore(mnemonics: mnemonics)
-    //        print(keystore.addresses)
-    //        Web3.default.keystoreManager = KeystoreManager([keystore])
-    //    }
+    func testCreateAccount() {
+        let mnemonics = web3swift.Mnemonics()
+        let keystore = try! BIP32Keystore(mnemonics: mnemonics)
+        print(keystore.addresses[0])
+        Web3.default.keystoreManager = web3swift.KeystoreManager([keystore])
+    }
+    
+    func testImportAccount() {
+        let mnemonicsString = "nation tornado double since increase orchard tonight left drip talk sand mad"
+        let mnemonics = try! web3swift.Mnemonics(mnemonicsString)
+        let keystore = try! BIP32Keystore(mnemonics: mnemonics)
+        print(keystore.addresses)
+        Web3.default.keystoreManager = web3swift.KeystoreManager([keystore])
+    }
     
     func testGetBalance() throws {
         let mnemonicsString = "nation tornado double since increase orchard tonight left drip talk sand mad"
-        let mnemonics = try! Mnemonics(mnemonicsString)
+        let mnemonics = try! chain3swift.Mnemonics(mnemonicsString)
         let keystore = try! BIP32Keystore(mnemonics: mnemonics)
-        print(keystore.addresses)
-        Chain3.default.keystoreManager = KeystoreManager([keystore])
+        print(keystore.addresses[0])
+        Chain3.default.keystoreManager = chain3swift.KeystoreManager([keystore])
     }
     
     func testGetAccounts() throws {
-        let mnemonics = Mnemonics()
+        let mnemonics = chain3swift.Mnemonics()
         let keystore = try! BIP32Keystore(mnemonics: mnemonics)
         print(keystore.addresses[0])
-        Chain3.default.keystoreManager = KeystoreManager([keystore])
+        Chain3.default.keystoreManager = chain3swift.KeystoreManager([keystore])
     }
 }
---
+++ walletcoreios.podspec
@@ -0,0 +1,18 @@
+Pod::Spec.new do |spec|
+    spec.name         = 'walletcoreios'
+    spec.version      = '0.0.1'
+    spec.ios.deployment_target = "8.0"
+    spec.osx.deployment_target = "10.10"
+    spec.tvos.deployment_target = "9.0"
+    spec.watchos.deployment_target = "2.0"
+    spec.license      = { :type => 'Apache License 2.0', :file => 'LICENSE.md' }
+    spec.summary      = 'walletcoreios implementation in pure Swift for iOS, macOS, tvOS, watchOS and Linux'
+    spec.homepage     = 'https://github.com/HFJingchuang/wallet_core_ios'
+    spec.author       = 'MYXU'
+    spec.source       = { :git => 'https://github.com/HFJingchuang/wallet_core_ios.git', :tag => spec.version }
+    spec.source_files = 'Sources/web3swift/**/*.swift,Sources/chain3swift/**/*.swift'
+    spec.swift_version = '4.2'
+    spec.dependency 'PromiseKit', '~> 6.4'
+    spec.dependency 'BigInt', '~> 3.1'
+    spec.dependency 'CryptoSwift', '~> 0.12'
+end
